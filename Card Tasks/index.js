4292a6a5984faef5e4a1a5795f71ee8983a2daba
kirillpanfile <kirillpanfile@gmail.com>
2021-12-11 10:47:49 +0200

    Added mounted() with parse from localstorage

--- Card Tasks/index.js
+++ Card Tasks/index.js
@@ -6,31 +6,36 @@ const app = Vue.createApp({
             newIcon: '',
             endTime: '',
             newColor: '',
-            time: ['0:00','1:00','2:00','3:00','4:00','5:00','6:00','7:00','8:00','9:00','10:0','11:0','12:0','13:0','14:0','15:0','16:0','17:0','18:0','19:0','20:0','21:0','22:0','23:0'],
+            time: ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:0', '11:0', '12:0', '13:0', '14:0', '15:0', '16:0', '17:0', '18:0', '19:0', '20:0', '21:0', '22:0', '23:0'],
             icons: [],
+            colors: [],
             tasks: [{
-                    time: '8:00',
-                    title: 'Working on SocialLens ',
-                    icon: '💻',
-                    hours: '8:00 - 9:00',
-                    color: 'bg-blue-500',
-                },
-                {
-                    time: '9:00',
-                    title: 'Design Team Standup ',
-                    icon: '✏',
-                    hours: '9:00 - 10:00',
-                    color: 'bg-yellow-600',
-                },
-                {
-                    time: '10:00',
-                    title: 'Take launch with the team ',
-                    icon: '🍽',
-                    hours: '10:00 - 11:00',
-                    color: 'bg-red-500',
-                }
+                time: '8:00',
+                title: 'Working on SocialLens ',
+                icon: '💻',
+                hours: '8:00 - 9:00',
+                color: 'bg-blue-500',
+            },
+            {
+                time: '9:00',
+                title: 'Design Team Standup ',
+                icon: '✏',
+                hours: '9:00 - 10:00',
+                color: 'bg-yellow-600',
+            },
+            {
+                time: '10:00',
+                title: 'Take launch with the team ',
+                icon: '🍽',
+                hours: '10:00 - 11:00',
+                color: 'bg-red-500',
+            }
             ]
         }
+    },
+    mounted() {
+        if (localStorage.getItem('tasks'))
+            this.tasks = JSON.parse(localStorage.getItem('tasks'))
     },
     methods: {
         addNewTask() {
@@ -41,7 +46,8 @@ const app = Vue.createApp({
                 obj.color = this.newColor
                 obj.hours = this.startTime + '-' + this.endTime
                 obj.time != '' && obj.title !='' && obj.icon != '' && obj.color != ''  ?  
-                this.tasks.push(obj) : void(0)
+                this.tasks.push(obj) : void (0)
+                localStorage.setItem('tasks',JSON.stringify(this.tasks))
         }
     },
 }).mount('#app')
