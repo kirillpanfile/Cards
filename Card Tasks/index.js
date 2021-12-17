9e92b72f45f0f24ec2cd8afb4607d171d46c78d5
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2021-12-17 09:27:47 +0200

    Major bug fixes

--- Card Tasks/index.js
+++ Card Tasks/index.js
@@ -7,8 +7,6 @@ const app = Vue.createApp({
             endTime: '',
             newColor: '',
             time: ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:0', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'],
-            icons: [''],
-            colors: [],
             tasks: [{
                 time: '8:00',
                 title: 'Working on SocialLens ',
@@ -33,6 +31,7 @@ const app = Vue.createApp({
             ]
         }
     },
+
     mounted() {
         if (localStorage.getItem('tasks'))
             this.tasks = JSON.parse(localStorage.getItem('tasks'))
